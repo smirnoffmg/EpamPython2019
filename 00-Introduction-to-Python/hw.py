143c7afb5d0de923fd35cce8a79a3b57bd38651c
Evgeniy-Afanasev <43401312+Evgeniy-Afanasev@users.noreply.github.com>
2019-05-19 20:32:33 +0300

    Update hw.py

--- 00-Introduction-to-Python/hw.py
+++ 00-Introduction-to-Python/hw.py
@@ -8,8 +8,8 @@
 """
 
 def is_permutation(a: str, b: str) -> bool:
-    # Нужно проверить, являются ли строчки 'a' и 'b' перестановками
+    if sorted(a) == sorted(b):
+        return True
+    else:
+        return False
 
-
-assert is_permutation('baba', 'abab')
-assert is_permutation('abbba', 'abab')
