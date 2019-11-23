0b13ea4ac8b66af4c35189deb2b05fc8c11f4b44
Vitomed <viktor.medyankin@mail.ru>
2019-11-23 20:27:51 +0300

    add Armstrong at hw3

--- 03-fp-decorator/hw/hw3.py
+++ 03-fp-decorator/hw/hw3.py
@@ -1,14 +1,27 @@
 from functools import reduce
 
 
-p6 = sum([i for i in range(1, 101)])**2 - sum([i**2 for i in range(1, 101)])
+# p6 = sum([i for i in range(1, 101)])**2 - sum([i**2 for i in range(1, 101)])
 # print(p6)
 
-p9 = [(a, b, c) for a in range(1001) for b in range(a) for c in range(b) if a**2 + b**2 + c**2 == 1000]
+# p9 = [(a, b, c) for a in range(1001) for b in range(a) for c in range(b) if a**2 + b**2 + c**2 == 1000]
 # print(p9)
 
-p40 = reduce(lambda x,y: x*y, [int(list("".join(str(i) for i in range(1, 1000001)))[x-1]) for x in [10**i for i in range(7)]])
+# p40 = reduce(lambda x,y: x*y, [int(list("".join(str(i) for i in range(1, 1000001)))[x-1]) for x in [10**i for i in range(7)]])
 # print(p40)
 
-p48 = [i**i for i in range(1, 1001)][-1:-11:-1]
+# p48 = [i**i for i in range(1, 1001)][-1:-11:-1]
 # print(p48)
+
+
+def return_armstrong_true_or_not(number: int) -> bool:
+
+    result = reduce(lambda sum, it: sum + pow(int(it), len(str(number))),
+                     str(number), 0)
+
+    if number == result:
+        return "{} is Armstrong number".format(number)
+    else:
+        return "{} is'nt Armstrong number".format(number)
+
+# print(return_armstrong_true_or_not(153))
