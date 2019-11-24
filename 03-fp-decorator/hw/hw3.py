27eccea32a3f95c128697ba9dcd7754cb41bb372
Vitomed <viktor.medyankin@mail.ru>
2019-11-24 18:38:28 +0300

    add collatz __doc__

--- 03-fp-decorator/hw/hw3.py
+++ 03-fp-decorator/hw/hw3.py
@@ -5,9 +5,10 @@ from functools import reduce
 # print(p6)
 
 # p9 = [(a, b, c) for a in range(1001) for b in range(a) for c in range(b) if a**2 + b**2 + c**2 == 1000]
-# print(p9)
+# print(p9)  # довести до ума!
 
-# p40 = reduce(lambda x,y: x*y, [int(list("".join(str(i) for i in range(1, 1000001)))[x-1]) for x in [10**i for i in range(7)]])
+# N = 10*6
+# p40 = reduce(lambda x,y: x*y, [int(list("".join(str(i) for i in range(1, N + 1)))[x-1]) for x in [10**i for i in range(7)]])
 # print(p40)
 
 # p48 = [i**i for i in range(1, 1001)][-1:-11:-1]
@@ -15,36 +16,47 @@ from functools import reduce
 
 
 def return_armstrong_true_or_not(number: int) -> bool:
+    """
+    Add anotation
+    :param number:
+    :return:
+    """
 
     result = reduce(lambda sum, it: sum + pow(int(it), len(str(number))),
-                     str(number), 0)
+                    str(number), 0)
 
     if number == result:
         return "{} is Armstrong number".format(number)
     else:
         return "{} is'nt Armstrong number".format(number)
 
+
 # print(return_armstrong_true_or_not(153))
 
-def collatz_steps(number: int, lst = []) -> int:
-    # if len(lst) == 0:
-    #     return []
-    if number == 1:
-        return number
-    if number % 2 == 0:
-        return collatz_steps(number // 2), number
-    return collatz_steps(3 * number + 1), number
 
-# list = []
-# list.append(collatz_steps(6))
-# print(list)
-# args = collatz_steps()
-# print(*args)
+count = -1
 
-def collaz_numb(num):
-    while num > 1:
-        num = (num // 2) if num % 2 == 0 else (3 * num + 1)
-        # print(num)
-    return num
 
-print(collaz_numb(5))
+def return_collatz_count_steps(number: int) -> int:
+    """Calculate the number of steps for count
+
+    collatz() that has one parameter named number.
+    If number is even, then collatz() should print
+    number // 2 and return this value. If number is
+    odd, then collatz() should print and return 3 * number + 1.
+    """
+    global count
+    count += 1
+    print("count_of_steps", count)
+    try:
+        number = int(number)
+    except ValueError:
+        print("Please enter a number correct!")
+    else:
+        if number == 1:
+            return number
+        if number % 2 == 0:
+            return return_collatz_count_steps(number // 2)
+        return return_collatz_count_steps(3 * number + 1)
+
+# print(return_collatz_count_steps("12"))
