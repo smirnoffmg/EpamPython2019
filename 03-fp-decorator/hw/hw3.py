d3f244511b8401f6620f6c1949f5f267aa033c00
Vitomed <viktor.medyankin@mail.ru>
2019-11-25 01:27:25 +0300

    p9 is done

--- 03-fp-decorator/hw/hw3.py
+++ 03-fp-decorator/hw/hw3.py
@@ -1,37 +1,45 @@
 from functools import reduce
 
+n = 100
+p6 = sum([i for i in range(1, n + 1)]) ** 2 - sum([i ** 2 for i in range(1, n + 1)])
+print(p6)
 
-# p6 = sum([i for i in range(1, 101)])**2 - sum([i**2 for i in range(1, 101)])
-# print(p6)
+""" Because of the ratio of a, b, c in a Pythagorean triplet no variable can ever be
+ larger than half the sum of a + b + c therefore I have lowered the upper limit to 500 or sum_elements // 2 """
 
-# p9 = [(a, b, c) for a in range(1001) for b in range(a) for c in range(b) if a**2 + b**2 + c**2 == 1000]
-# print(p9)  # довести до ума!
+sum_elements = 1000
+half_sum = sum_elements // 2
+p9 = [(a, b, 1000 - (a + b)) for a in range(1, half_sum) for b in range(a + 1, half_sum) if
+      a ** 2 + b ** 2 == (sum_elements - (a + b)) ** 2]
+print("a = {0}, b = {1}, c = {2}".format(*p9[0]))
 
-# N = 10*6
-# p40 = reduce(lambda x,y: x*y, [int(list("".join(str(i) for i in range(1, N + 1)))[x-1]) for x in [10**i for i in range(7)]])
-# print(p40)
+N = 10*6
+p40 = reduce(lambda x, y: x*y, [int(list("".join(str(i) for i in range(1, N + 1)))[x-1])
+                               for x in [10**i for i in range(7)]])
+print(p40)
 
-# p48 = [i**i for i in range(1, 1001)][-1:-11:-1]
-# print(p48)
+
+p48 = [i ** i for i in range(1, 1001)][-1:-11:-1]
+print(p48)
 
 
 def return_armstrong_true_or_not(number: int) -> bool:
-    """
-    Add anotation
-    :param number:
-    :return:
+    """Function determines the Armstrong number in functional form
+    and return result - True or False
+
+    Armstrong's number is a natural number, which in a given number
+    system is equal to the sum of its digits raised to a power equal
+    to the number of its digits.
     """
 
-    result = reduce(lambda sum, it: sum + pow(int(it), len(str(number))),
+    result = reduce(lambda x, y: x + pow(int(y), len(str(number))),
                     str(number), 0)
-
     if number == result:
-        return "{} is Armstrong number".format(number)
-    else:
-        return "{} is'nt Armstrong number".format(number)
+        return number
 
 
-# print(return_armstrong_true_or_not(153))
+number = 153
+assert return_armstrong_true_or_not(number) == number
 
 
 def return_collatz_count_steps(number: int, count=None) -> int:
