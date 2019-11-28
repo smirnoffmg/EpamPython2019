b96e65d496310ba955274be6616d43aa1b6fbad7
andrejshulaev <andrejshulaev@gmail.com>
2019-11-28 21:52:29 +0300

    [Lecture 7] restore hw from lecture 6

--- 06-advanced-python/hw/task2.py
+++ 06-advanced-python/hw/task2.py
@@ -1,36 +1,16 @@
 """
-Написать свое property c кэшем и таймаутом
-полностью повторяет поведение стандартной property за исключением:
-    * хранит результат работы метода некоторое время, которое передается
-      параметром в инициализацию проперти
-    * пересчитывает значение, если таймер истек
+Реализовать класс Quaternion, позволяющий работать с кватернионами
+https://ru.wikipedia.org/wiki/%D0%9A%D0%B2%D0%B0%D1%82%D0%B5%D1%80%D0%BD%D0%B8%D0%BE%D0%BD
+Функциональность (магическими методами):
+- сложение
+- умножение
+- деление
+- сравнение
+- нахождение модуля
+- строковое представление и repr
+По желанию:
+- взаимодействие с числами других типов
 """
 
-import time
-import uuid
-
-
-class Message:
-
-    @timer_property(t=10)
-    def msg(self):
-        self._msg = self.get_message()
-        return self._msg
-
-    @msg.setter # reset timer also
-    def msg(self, param):
-        self._msg = param
-
-    def get_message(self):
-        """
-        Return random string
-        """
-        return uuid.uuid4().get_hex()
-
-
-if __name__ == '__main__':
-    m = Message()
-    initial = m.read
-    assert initial is m.read
-    time.sleep(10)
-    assert initial is not m.read
+class Quaternion:
+    pass
