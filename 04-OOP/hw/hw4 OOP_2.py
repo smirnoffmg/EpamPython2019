05972766f66cc184f4ba60bd78a4badf08abbbf0
Vitomed <viktor.medyankin@mail.ru>
2019-11-29 13:23:08 +0300

    hw4 OOP first task2 decorator saver (1 inner layer without 2 inner layer was before)

--- 04-OOP/hw/hw4 OOP_2.py
+++ 04-OOP/hw/hw4 OOP_2.py
@@ -1,34 +1,20 @@
 import functools
-from functools import wraps
 
 
-def saver(deco_arg):
-
-    def decorator(func):
-        # @wraps(func)
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
-
-        wrapper.__name__ = func.__name__
-        wrapper.__doc__ = func.__doc__
-        wrapper.__original_func = deco_arg
-
-        return wrapper
+def saver(func):
+    def decorator(wraps):
+        wraps.__name__ = func.__name__
+        wraps.__doc__ = func.__doc__
+        wraps.__original_func = func
+        return wraps
     return decorator
 
 
-# @saver
-# def func(*args, **kwargs):
-#     return sum(args)
-
-
 def print_result(func):
-    # Place for new decorator
     @saver(func)
     def wrapper(*args, **kwargs):
         """Function-wrapper which print result of an original function"""
         result = func(*args, **kwargs)
-        print(result)
         return result
     return wrapper
 
@@ -42,10 +28,7 @@ def custom_sum(*args):
 if __name__ == '__main__':
     custom_sum([1, 2, 3], [4, 5])
     custom_sum(1, 2, 3, 4)
-
-    print(custom_sum.__doc__)
-    print(custom_sum.__name__)
     without_print = custom_sum.__original_func
-
     # the result returns without printing
     without_print(1, 2, 3, 4)
+
