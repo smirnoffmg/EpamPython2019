3cdc2031e7b084eabc968b1f1048b962c3f5ac31
Vitomed <viktor.medyankin@mail.ru>
2019-12-01 02:47:32 +0300

    hw3.4 (Last update task) no the need to write a wrapper ourselves each time.

--- 03-fp-decorator/hw/hw3.4 (Last ubdate task).py
+++ 03-fp-decorator/hw/hw3.4 (Last ubdate task).py
@@ -1,42 +1,21 @@
-# Написать applydecorator который работает так:
 
-def applydecorator(name):
 
-    def wrapper(*args):
-        func(args)
-    return wrapper
+def applydecorator(func):
+    def inner(f, *args, **kwargs):
+        func(f, *args, *kwargs)
+        return f
+    return inner
+
 
 @applydecorator
 def saymyname(f, *args, **kwargs):
     print('Name is', f.__name__)
     return f(*args, **kwargs)
 
-# @saymyname
 
+@saymyname
 def foo(*whatever):
     return whatever
 
-print(*foo(1,2,3))
-
-# saymyname(*foo(1,2,3))
-#
-#
-# def decorator_function(func):
-#     def wrapper():
-#         print('Функция-обёртка!')
-#         print('Оборачиваемая функция: {}'.format(func))
-#         print('Выполняем обёрнутую функцию...')
-#         wrapper
-#         print('Выходим из обёртки')
-#     return func
-#
-# @decorator_function
-# def hello_world():
-#     return "Hello world"
-#
-# print(hello_world())
-#
-# @hello_world
-# def goodby_world():
-#     print("goodby_world")
+print(*foo(40, 2))
 
