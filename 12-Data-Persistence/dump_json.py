65130a1cc2a4e2e4f9042e9932fd44c641c29512
Rasul Osmanov <orasul@mail.ru>
2019-12-26 14:34:17 -0500

    Added data persistance lecture

---
+++ 12-Data-Persistence/dump_json.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+
+data = {}
+data['people'] = []
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+data['people'].append({
+    'name': 'Larry',
+    'website': 'google.com',
+    'from': 'Michigan'
+})
+data['people'].append({
+    'name': 'Tim',
+    'website': 'apple.com',
+    'from': 'Alabama'
+})
+
+with open('data.txt', 'w') as outfile:
+    json.dump(data, outfile)
+
+with open('data.txt', 'rb') as infile:
+    data2 = json.load(infile)
+
+print(data2)
